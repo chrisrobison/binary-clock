dacd72ec028fbe61115516aa337956fadd37ea1f
Robison, Christopher <cdr@netoasis.net>
2016-07-08 17:09:37 -0700

    Refactored to use single LED image for all LED's, regardless of state, color or position. The LED image is included as a base64 encoded data url in style.css. Fixed issue with inconsistant clearing of LED's.

--- binary-clock.js
+++ binary-clock.js
@@ -9,6 +9,7 @@
    };
    var parts = ['h','h','m','m','s','s'];
    var layout = [2,4,3,4,3,4];
+   var leds = [];
 
    function $(id) {
       return document.getElementById(id);
@@ -33,8 +34,8 @@
             for (var bit=0; bit<4; bit++) {
                if (tstr[x] & Math.pow(2,bit)) {
                   var id = parts[x] + ((x%2)+1) + "_" + (bit + 1);
-                  console.log(id);
-                  $(id).src = "led-on.png";
+                  console.log(id + ": led on " + parts[x]);
+                  $(id).className = "led on " + parts[x];
                }
             }
          }
@@ -48,38 +49,64 @@
    }
    
    function alloff() {
-      var leds = document.getElementsByClassName('led');
-
-      for (var led in leds) {
-         leds[led].src = "led-off.png";
+      var leds = document.getElementsByClassName("led");
+      for (var led=0; led<leds.length; led++) {
+         leds[led].className = 'led off no_' + leds[led].id[0];
       }
 
    }
 
    function init() {
       var container = $("clock"),
-          out = $("clockbody").innerHTML;
+          out = $("clockbody").innerHTML,
+          tr, td, wrap;
       
+      /**
+       * Build LED table:
+       *
+       *     X  X  8
+       *  X XX XX  4
+       * XX XX XX  2
+       * XX XX XX  1
+       * HH:MM:SS 
+       *
+       *
+       * Bottom row represents our least significant digit so row counter counts down.
+       **/
       for (var row=4; row>0; row--) {
-         out += "<tr>";
-         out += "<td class='bit'>" + Math.pow(2,row - 1) + "</td>";
-         for (var col=0; col<6; col++) {
-            if (layout[col] > row-1) {
-               out += "<td><img class='led' id='" + parts[col] + ((col%2)+1) + "_" + row +"' src='led-off.png'></td>";
-            } else {
-               out += "<td></td>";
-            }
-         }
-         out += "<td class='bit'>" + Math.pow(2,row - 1) + "</td>";
-         out += "</tr>\n";
-      }
-      out += "<tr>";
-      for (var i=0; i<8; i++) {
-         out += "<td class='digits' id='col" + (i+1) + "'></td>";
-      }
-      out += "</tr>";
+         tr = document.createElement('tr');
+         td = document.createElement('td');
+         td.className = "bit";
+         td.innerHTML = Math.pow(2,row - 1);
+         tr.appendChild(td);
 
-      clock.innerHTML = out;
+         for (var col=0; col<6; col++) {
+            td = document.createElement('td');
+            wrap = document.createElement('div');
+            if (layout[col] > row-1) {
+               wrap.className = "led no_" + parts[col];
+               wrap.id = parts[col] + ((col%2)+1) + "_" + (row);
+               td.appendChild(wrap);
+            }
+            tr.appendChild(td);
+         }
+         td = document.createElement('td');
+         td.className = 'bit';
+         td.innerHTML = Math.pow(2,row - 1);
+         tr.appendChild(td);
+         $("clockbody").appendChild(tr);
+      }
+      
+      tr = document.createElement('tr');
+      
+      for (var i=0; i<8; i++) {
+         td = document.createElement('td');
+         td.className = 'digits';
+         td.id = 'col' + (i+1);
+         tr.appendChild(td);
+      }
+      $("clockbody").appendChild(tr);
+
       setTimeout(updateClock, 1000);
    }
    init();
